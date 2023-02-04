f787d99622eb390b191a6cd4b7aad40394420753
MihailRis <mexaruc@gmail.com>
2023-02-04 18:50:10 +0300

    Version updated from 2.1.2 to 2.2.0

--- src/mihailris/mio/Disk.java
+++ src/mihailris/mio/Disk.java
@@ -12,8 +12,8 @@ import java.util.Properties;
 public class Disk {
     private static long TEMP_ID = 0;
     public static final int VERSION_MAJOR = 2;
-    public static final int VERSION_MINOR = 1;
-    public static final int VERSION_PATCH = 2;
+    public static final int VERSION_MINOR = 2;
+    public static final int VERSION_PATCH = 0;
     public static final String VERSION_STRING = VERSION_MAJOR+"."+VERSION_MINOR+"."+VERSION_PATCH;
     public static long totalRead;
     public static long totalWrite;
