6451624fc1427a05980b98e7c32c7425223ed72e
ubuntu <mexaruc@gmail.com>
2023-03-06 12:01:36 +0300

    Minor Test change

--- src/mihailris/mio/tests/Test.java
+++ src/mihailris/mio/tests/Test.java
@@ -4,9 +4,6 @@ import mihailris.mio.Disk;
 import mihailris.mio.IORandomAccess;
 import mihailris.mio.MemoryDevice;
 
-import java.io.DataOutputStream;
-import java.io.OutputStream;
-
 public class Test {
     public static void main(String[] args) throws Exception {
         Disk.createAbsDevice();
@@ -14,8 +11,7 @@ public class Test {
         MemoryDevice device = new MemoryDevice();
         try (IORandomAccess raf = device.openRandomAccess("test.fs", true)) {
             raf.write('A');
-            new DataOutputStream((OutputStream) raf).writeUTF("Some example Text");
-            raf.position(2);
+            raf.position(0);
             System.out.println((char)raf.read());
             System.out.println(raf.available());
         }
